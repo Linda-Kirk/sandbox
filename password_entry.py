0bded43845220b5fd72403711d0c5d63656e9cb6
Linda-Kirk <linda.kirk@my.jcu.edu.au>
2019-08-16 11:59:10 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,16 @@
 """
 Linda Kirk
+Program will ask user for a password, with error-checking to repeat if the password doesn't meet a
+minimum length set by a variable.
+Will print asterisks as long as the password.
+Example: if the user enters "Pythonista" (10 characters), the program should print "**********".
 """
+MINIMUM_LENGTH = 10
+
+password = input('Enter a password (must be at least {} characters in length): '.format(MINIMUM_LENGTH))
+while len(password) < MINIMUM_LENGTH:
+    print('Password is too short!')
+    password = input('Enter a password (must be at least {} characters in length): '.format(MINIMUM_LENGTH))
+print('*' * len(password))
+
+
